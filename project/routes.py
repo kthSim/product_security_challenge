ae2d8e3516c02fd09bbb4823345bc59f8d8dc197
kthSim <kth_sim@hotmail.com>
2019-09-11 17:03:54 +0800

    Created semi-workable log in page (flow only, no database yet)

--- project/routes.py
+++ project/routes.py
@@ -1,12 +1,18 @@
 from project import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from project.forms import LoginForm
 
 
 @app.route("/")
 def index():
-    return "Hello World! Zendesk :D"
+    return render_template("base.html", title='Zendesk Security Challenge')
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("index.html")
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login Requested for user {}, remember_me={}'.format(form.username.data, form.rmbr_user.data))
+        return redirect(url_for('index'))
+    return render_template("login.html", title="Log In", form=form)
